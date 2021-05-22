46591635be3dcb46dc7250e00640fb29c5dfed9d
Jorge Vicente Hernandez <jorge.vicente@rimac.com.pe>
2021-05-21 23:47:58 -0500

    git ignore

--- get_cod.py
+++ get_cod.py
@@ -1,7 +1,12 @@
 import requests
+import json
 
 URL = "https://dataondemand.bvl.com.pe/v1/stock-quote/home"
+var_payload = {"sector": "", "isToday": "true", "companyCode": "", "inputCompany": ""}
+
 
 
 if __name__ == "__main__":
-    print("Hola")
+    r = requests.post(URL, data=json.dumps(var_payload))
+    lista_codigos = json.loads(r.text)
+    print(lista_codigos[0])
